c5403aecd40cac6e8e3cb61ad11f02ef607b5188
Joyce T. <134914215+thelovelightliveventure@users.noreply.github.com>
2025-01-20 20:42:26 +0545

    Update word.py

--- word.py
+++ word.py
@@ -1,5 +1,5 @@
 # SETUP
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 letternumbers = []
 wordarray = []
 totalnumbers = 0
