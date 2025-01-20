d217cd4881df9c379c1cb19bcce9fe2b4d35bdb5
Joyce T. <134914215+thelovelightliveventure@users.noreply.github.com>
2025-01-20 20:43:44 +0545

    Update paragraphs.py

--- paragraphs.py
+++ paragraphs.py
@@ -26,8 +26,6 @@ def dollarwords(input):
             if wordarray[i] == alphabet[j]:
                 j += 1
                 letternumbers.append(j)
-        if wordarray[i] == "y":
-            letternumbers.append(25)
 
     # FIND TOTAL VALUE OF WORD
     for i in range(0, len(letternumbers)):
